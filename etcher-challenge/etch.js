6f584d74d275e415453047f070b2422b0c7f2d38
talexcrowell <talexcrowell@gmail.com>
2018-09-27 12:06:30 -0700

    added etcher challenge

---
+++ etcher-challenge/etch.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Add Event Listeners here:
+function addHoverListeners () {
+  $('.cell').hover(event => {
+    $(event.target).addClass('active');
+  });
+}
+
+function addResetGridHandler() {
+  $('.controls button').click(event => {
+    createAndPlaceRows(8);
+    addHoverListeners();
+  });
+}
+
+// When DOM is ready:
+$(() => {
+  createAndPlaceRows(8);
+
+  // Bind your event listeners here:
+  addHoverListeners();
+  addResetGridHandler();
+});
+
+
+
+
+
+
+
+// ===============================
+// Code below is to automate grid creation
+// You can ignore this!
+// ===============================
+function createRow(cellCount) {
+  let row = '<div class="row">';
+  let str = '<div class="cell">&nbsp;</div>';
+  for (let i = 0; i < cellCount; i++) {
+    row += str;
+  }
+  row += '</div>';
+  return row;
+}
+
+function createRows(n) {
+  let rows = '';
+  for (let i = 0; i < n; i++) {
+    rows += createRow(n);
+  }
+  return rows;
+}
+
+function createAndPlaceRows(n) {
+  $('.cell').remove(); // Clear all current cells and listeners
+  const rows = createRows(n);
+  $('.grid').html(rows);
+  const cells = $('.cell');
+  cells.css({ height: cells.width() });
+}

5ebf68e49df67bade1e5fc2b885b2357ec5ad548
Arun Seehra <seehra.a@gmail.com>
2018-09-27 13:47:26 -0500

    Add the shopping list implementation

--- shopping-list/index.js
+++ shopping-list/index.js
@@ -1,6 +1,36 @@
 'use strict';
 
 function main() {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    const input = $('.js-shopping-list-entry').val();
+    // Create a new shopping list item
+    const newItem = `
+      <li>
+        <span class="shopping-item">${input}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button
+        </div>
+      </li>`;
+    // Append new item to the shoppingList
+    $('ul.shopping-list').append(newItem);
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+    const listItemRoot = $(this).closest('li');
+    const itemSpan = listItemRoot.find('.shopping-item');
+    itemSpan.toggleClass('shopping-item__checked');
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+    const itemRoot = $(this).closest('li');
+    itemRoot.remove();
+  });
 }
 
 $(main);
